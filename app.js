7723aa0908d9f3c70214168ea04b6ac597c59c86
vasya <ololohkalool@gmail.com>
2020-08-03 18:37:57 +0300

    write code to preloader for relod page

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const video = document.querySelector('.video-container'),
 	  btn = document.querySelector('.switch-btn');
 
-
-
-
-
-
-
-
+window.addEventListener('load', () => {
+	const preloader = document.querySelector('.preloader');
+	preloader.classList.add('hidden');
+});
